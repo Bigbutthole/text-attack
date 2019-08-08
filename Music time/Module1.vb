364601e51da903fa285bef5955446cb489ccf007
Bigbutt <313947568@qq.com>
2019-08-08 16:52:13 +0800

    rebuild bug

--- Music time/Module1.vb
+++ Music time/Module1.vb
@@ -10,12 +10,12 @@ Module Module1
     Public list3 As New List(Of FontStyle) From {FontStyle.Regular, FontStyle.Italic, FontStyle.Bold, FontStyle.Underline, FontStyle.Strikeout}
     Public Random As New Random '定义取随机值函数(性能比rnd好，函数不消耗CPU)
     Sub Main()
+        Dim screen As Graphics = Graphics.FromHdc(GetWindowDC(GetDesktopWindow()))
         Do
             Dim msg As String = list.Item(Random.Next(list.Count - 1)）
             Dim color As Brush = list2.Item(Random.Next(list2.Count - 1))
             Dim fontsyle As FontStyle = list3.Item(Random.Next(list3.Count - 1))
             Threading.Thread.Sleep(100)
-            Dim screen As Graphics = Graphics.FromHdc(GetWindowDC(GetDesktopWindow()))
             Dim rect As Rectangle = Windows.Forms.Screen.PrimaryScreen.WorkingArea '获得屏幕大小
             Dim font As Font = New Font(SystemFonts.MessageBoxFont.FontFamily, Random.Next(1, 25), fontsyle) '设置字体大小取随机值
             screen.DrawString(msg, font, color, New PointF(Random.Next(rect.Width), Random.Next(rect.Height)))
